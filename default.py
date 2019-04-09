91d9e285f4f1959d90c8b935d0f71b31a859f8ff
Osvaldo Gioia <osvaldogioia@hotmail.com>
2019-04-09 00:44:47 -0300

    Update default.py

--- default.py
+++ default.py
@@ -280,7 +280,7 @@ def m3uCategory(url, logos, cache, gListIndex=-1):
 def PlayUrl(name, url, iconimage=None):
     if url.startswith('acestream://'):
        url = 'plugin://program.plexus/?mode=1&url={0}&name={1}&iconimage={2}'.format(url, name, iconimage)
-    if url.startswith('https://www.youtube.com'):
+    elif url.startswith('https://www.youtube.com'):
        auxurl = url[url.find('v=') + 2:]
        url = 'plugin://plugin.video.youtube/play/?video_id={0}'.format(auxurl)
     else:
